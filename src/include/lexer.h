fff9e30ad42bb1918b166db263318efff2f2557d
edwinmoradian90 <edwin.moradian90@gmail.com>
2020-11-10 16:21:51 +0400

    finished basic lexer for basics tokens

--- src/include/lexer.h
+++ src/include/lexer.h
@@ -14,6 +14,8 @@ void lexer_advance(lexer_T* lexer);
 void lexer_skip_white_space(lexer_T* lexer);
 token_T* lexer_get_next_token(lexer_T* lexer);
 token_T* lexer_collect_string(lexer_T* lexer);
+token_T* lexer_collect_id(lexer_T* lexer);
+token_T* lexer_advance_with_token(lexer_T* lexer, token_T* token);
 char* lexer_get_current_char_as_string(lexer_T* lexer);
 
 #endif
